42810a38519da6523d266c89dd7ac61b4fadc9fd
Akjol <Akjol.igm@gmail.com>
2020-10-20 04:59:12 -0500

    Spring Boot init

---
+++ shoppingcart-service/src/main/java/com/pm/ecommerce/shoppingcartservice/ShoppingcartServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pm.ecommerce.shoppingcartservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingcartServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingcartServiceApplication.class, args);
+	}
+
+}
