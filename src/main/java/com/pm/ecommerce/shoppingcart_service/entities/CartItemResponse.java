ed1e97212d57eb979ed20795ba2d613cd34b2ddc
Akjol <Akjol.igm@gmail.com>
2020-10-28 17:20:09 -0500

    CartItemResponse quick update

--- src/main/java/com/pm/ecommerce/shoppingcart_service/entities/CartItemResponse.java
+++ src/main/java/com/pm/ecommerce/shoppingcart_service/entities/CartItemResponse.java
@@ -1,15 +1,22 @@
 package com.pm.ecommerce.shoppingcart_service.entities;
 
 import com.pm.ecommerce.entities.CartItem;
+import com.pm.ecommerce.entities.CartItemAttribute;
 import lombok.Data;
+
+import java.util.Set;
 
 @Data
 public class CartItemResponse {
     private int id;
     private int quantity;
+    private String name;
+    private Set<CartItemAttribute> cartItemAttributes;
 
     public CartItemResponse(CartItem cartItem){
         this.id = cartItem.getId();
         this.quantity = cartItem.getQuantity();
+        this.name = cartItem.getProduct().getName();
+        this.cartItemAttributes = cartItem.getAttributes();
     }
 }
