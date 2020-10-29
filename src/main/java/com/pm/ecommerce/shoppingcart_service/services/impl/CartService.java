0f77056525d1f908c541e130a7e8236abdd58c59
Akjol <Akjol.igm@gmail.com>
2020-10-29 13:00:54 -0500

    Fixed errors

--- src/main/java/com/pm/ecommerce/shoppingcart_service/services/impl/CartService.java
+++ src/main/java/com/pm/ecommerce/shoppingcart_service/services/impl/CartService.java
@@ -3,10 +3,12 @@ package com.pm.ecommerce.shoppingcart_service.services.impl;
 import com.pm.ecommerce.entities.*;
 import com.pm.ecommerce.enums.ProductStatus;
 import com.pm.ecommerce.enums.VendorStatus;
+import com.pm.ecommerce.shoppingcart_service.entities.CartItemRequest;
 import com.pm.ecommerce.shoppingcart_service.entities.CartItemResponse;
 import com.pm.ecommerce.shoppingcart_service.entities.CartResponse;
 import com.pm.ecommerce.shoppingcart_service.repositories.CartRepository;
 import com.pm.ecommerce.shoppingcart_service.repositories.CartItemRepository;
+import com.pm.ecommerce.shoppingcart_service.repositories.ProductRepository;
 import com.pm.ecommerce.shoppingcart_service.services.ICartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,12 +20,14 @@ public class CartService implements ICartService {
 
     private CartRepository cartRepository;
     private CartItemRepository cartItemRepository;
+    private ProductRepository productRepository;
 
 
     @Autowired
-    public CartService(CartRepository cartRepository, CartItemRepository cartItemRepository){
+    public CartService(CartRepository cartRepository, CartItemRepository cartItemRepository, ProductRepository productRepository){
         this.cartRepository = cartRepository;
         this.cartItemRepository = cartItemRepository;
+        this.productRepository = productRepository;
     }
 
     @Override
@@ -55,29 +59,40 @@ public class CartService implements ICartService {
     }
 
     @Override
-    public CartResponse addToCart(CartItem item, String sessionId) throws Exception {
+    public CartItemResponse addToCart(CartItemRequest item, String sessionId) throws Exception {
+        if(item == null) throw new Exception("Item should be not null");
         Cart cart = cartRepository.findBySessionId(sessionId).orElse(null);
         if(cart == null) throw new Exception("Cart Not Found");
         //Checking Product validation
-        Product product = item.getProduct();
-        if (product.getStatus() != ProductStatus.PUBLISHED.PUBLISHED) throw new Exception("Product not Valid");
+        Product product = productRepository.findById(item.getProductId()).orElse(null);
+        if (product.getStatus() != ProductStatus.PUBLISHED) throw new Exception("Product not Valid");
         //Checking Vendor status
         Vendor vendor = product.getVendor();
         if (vendor.getStatus() != VendorStatus.APPROVED) throw new Exception("Vendor status not Approved");
         //Checking Vendor status
         Category category = product.getCategory();
         if (category.isDeleted()) throw new Exception("Category is not available");
+        //Setting CartItem object
+        CartItem cartItem = new CartItem();
+        cartItem.setQuantity(item.getQuantity());
+        cartItem.setRate(item.getRate());
+        cartItem.setProduct(product);
+        cartItem.setAttributes(item.getAttributes());
+        //Adding CartItems
         Set<CartItem> cartItems = cart.getCartItems();
-        cartItems.add(item);
-        return new CartResponse(cartRepository.save(cart));
+        cartItems.add(cartItem);
+        cartRepository.save(cart);
+        return new CartItemResponse(cartItemRepository.save(cartItem));
     }
 
-    public CartResponse updateProduct(CartItem item, String sessionId) throws Exception {
+    public CartItemResponse updateCartItem(CartItemRequest item, String sessionId) throws Exception {
         Cart cart = cartRepository.findBySessionId(sessionId).orElse(null);
         if(cart == null) throw new Exception("Cart Not Found");
+        CartItem cartItem = cartItemRepository.findById(item.getId()).orElse(null);
+        if(cartItem == null) throw new Exception("Cart Item Not Found");
         //Checking Product validation
-        Product product = item.getProduct();
-        if (product.getStatus() != ProductStatus.PUBLISHED.PUBLISHED) throw new Exception("Product not Valid");
+        Product product = productRepository.findById(item.getProductId()).orElse(null);
+        if (product.getStatus() != ProductStatus.PUBLISHED) throw new Exception("Product not Valid");
         //Checking Vendor status
         Vendor vendor = product.getVendor();
         if (vendor.getStatus() != VendorStatus.APPROVED) throw new Exception("Vendor status not Approved");
@@ -90,16 +105,19 @@ public class CartService implements ICartService {
                 i.setQuantity(i.getQuantity() + item.getQuantity());
             }
         }
-        return new CartResponse(cartRepository.save(cart));
+        cartRepository.save(cart);
+        return new CartItemResponse(cartItemRepository.save(cartItem));
     }
 
     @Override
-    public CartItemResponse deleteProduct(CartItem item, String sessionId) throws Exception {
+    public CartItemResponse deleteCartItem(int cartItemId, String sessionId) throws Exception {
         Cart cart = cartRepository.findBySessionId(sessionId).orElse(null);
         if(cart == null) throw new Exception("Cart Not Found");
         //Checking Product validation
+        CartItem item = cartItemRepository.findById(cartItemId).orElse(null);
+        if(item == null) throw new Exception("Cart Item Not found");
         Product product = item.getProduct();
-        if (product.getStatus() != ProductStatus.PUBLISHED.PUBLISHED) throw new Exception("Product not Valid");
+        if (product.getStatus() != ProductStatus.PUBLISHED) throw new Exception("Product not Valid");
         //Checking Vendor status
         Vendor vendor = product.getVendor();
         if (vendor.getStatus() != VendorStatus.APPROVED) throw new Exception("Vendor status not Approved");
